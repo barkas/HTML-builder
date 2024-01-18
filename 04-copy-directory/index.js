87ebb29371a51c9cf4b5e4c0272ee7b316285995
barkas <vineda.contact@gmail.com>
2024-01-18 16:07:18 +0200

    fix: add check for the first run, in case directory do not exist

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,7 +5,13 @@ const inputdir = path.join(__dirname, 'files');
 const outputdir = path.join(__dirname, 'files-copy');
 
 const copyDir = async function (input, output) {
-  await fs.rm(output, { recursive: true });
+  try {
+    await fs.access(output);
+    await fs.rm(output, { recursive: true });
+  } catch (err) {
+    // nothing to do, directory will be created
+  }
+
   await fs.mkdir(output, { recursive: true }, (err) => {
     if (err) return console.error(err);
   });
