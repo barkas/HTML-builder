7d1892769fef931df7eba04b884d42df4d335215
barkas <vineda.contact@gmail.com>
2024-01-18 15:54:19 +0200

    feat: implement 04-copy-directory task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const inputdir = path.join(__dirname, 'files');
+const outputdir = path.join(__dirname, 'files-copy');
+
+const copyDir = async function (input, output) {
+  await fs.rm(output, { recursive: true });
+  await fs.mkdir(output, { recursive: true }, (err) => {
+    if (err) return console.error(err);
+  });
+  const files = await fs.readdir(input);
+  for (const file of files) {
+    try {
+      await fs.copyFile(path.resolve(input, file), path.resolve(output, file));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
+
+copyDir(inputdir, outputdir);
